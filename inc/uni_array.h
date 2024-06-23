9599ce8d0b88bb651e020e6b79fe38ead107fab7
xcs <evtjk@hotmail.com>
2024-06-23 20:17:20 +0800

    4th commit 我就喜欢commit

--- inc/uni_array.h
+++ inc/uni_array.h
@@ -13,10 +13,12 @@ public:
     unsigned int m_Capacity;
     // unsigned int ; 
     Uni_array(int );
+    Uni_array(class Uni_array<T> & n);
+
     ~Uni_array();
     void show(int);
     T& operator[] (int index);
-
+    void operator= (Uni_array&);
 private:
     T& visit(int index);
 
@@ -27,13 +29,27 @@ template <class T>
 Uni_array<T>::Uni_array(int n)
 {
     m_Capacity = n;
-    m_ptr =  new T[n]();/*分配堆空间并赋初值为0；*/
+    m_ptr =  new T[n]();        /*分配堆空间并赋初值为0；*/
+}
+
+template <class T>
+Uni_array<T>::Uni_array(Uni_array & uarr)    /**/
+{
+    this->m_Capacity = uarr.m_Capacity;
+    m_ptr =  new T[uarr.m_Capacity]();        /*深拷贝 申请另一块堆区内存*/
+    for(int i=0;i<uarr.m_Capacity;i++)
+    {
+        m_ptr[i] = uarr[i];         /*mptr是指针。没有运算符重载。uarr是类引用。发生运算符重载*/
+    }
+    printf("拷贝完辣！\n");
 }
 
 template <class T>
 Uni_array<T>::~Uni_array()
 {
-    delete[] this->m_ptr;
+    if(this->m_ptr != nullptr)
+        delete[] this->m_ptr;
+    this->m_ptr = nullptr;
 }
 template <class T>
 void Uni_array<T>::show(int n)
@@ -55,4 +71,16 @@ T& Uni_array<T>::operator[] (int index)
     return m_ptr[index];
 }
 
-#define UNI_ARRAY_CPP__
+template <class T>
+void Uni_array<T>::operator= (Uni_array& uarr)
+{
+    this->m_Capacity = uarr.m_Capacity;
+    m_ptr =  new T[uarr.m_Capacity]();        /*深拷贝 申请另一块堆区内存*/
+    for(int i=0;i<uarr.m_Capacity;i++)
+    {
+        m_ptr[i] = uarr[i];         /*mptr是指针。没有运算符重载。uarr是类引用。发生运算符重载*/
+    }
+    printf("拷贝完辣！\n");
+
+    //return *this;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,32 @@
 #include <string>
 
 #include "uni_array.h"
+#define XNEWL__ printf("\n")
 
 int main()
 {
-    Uni_array<int> a1(5);
-    a1.show(5);
-    a1[2]+=1;
-    printf("\n%d\n",a1.[2]);
+    {
+        Uni_array<int> a1(5);
+        a1.show(5);
+        a1[2]+=1;
+        printf("\n%d\n",a1[2]);
+
+        XNEWL__;
+        Uni_array a2(a1);
+        a2.show(5);
+        a2[1] = 2;
+
+        a1 = a2;
+        a1.show(5);
+    }
+        XNEWL__;
+    // {
+    //     Uni_array<float> a1(6);
+    //     a1.show(6);
+    //     a1[3]+=1;
+    //     printf("\n%f\n",a1[3]);
+    // }
+
     return 0;
 }
 
