9e215a3d0f7f49c5003e31100fad87ebc1742ce1
xcs <evtjk@hotmail.com>
2024-06-24 01:02:03 +0800

    fuck. i will do it tomorrow.

--- inc/uni_array.h
+++ inc/uni_array.h
@@ -19,6 +19,9 @@ public:
     void show();
     T& operator[] (int index);
     Uni_array& operator= (Uni_array&);
+
+    void append_n(int);
+    void delete_n(int);
 private:
     T& visit(int index);
 
@@ -89,3 +92,52 @@ Uni_array<T>& Uni_array<T>::operator= (Uni_array& uarr)     /*总感觉有点安
 
     return *this;
 }
+
+template <class T>  /*尾插 n个*/
+void Uni_array<T>::append_n(int extra_n)
+{
+    unsigned int new_capacity = this->m_Capacity + extra_n;
+    T* new_ptr;
+    new_ptr = new T[new_capacity]();        /*申请堆区空间 后赋初值为0*/
+    for(int i;i<this->m_Capacity;i++){
+        new_ptr[i] = this->m_ptr[i];        /*逐个复制之前数组的元素*/
+    }
+
+    this->m_Capacity = new_capacity;
+    delete[] this->m_ptr;                   /*如果不删除 一定会造成内存泄漏*/
+    this->m_ptr = new_ptr;
+
+}
+
+template <class T>
+void Uni_array<T>::delete_n(int delete_n)       /*尾删n个*/     /*我突然有个问题。类模板里面能不能有函数模板做成员函数。*/ /*搜了一下 可以*/
+{                                                      
+    if(!delete_n)
+        return;
+    int new_capacity = this->m_Capacity - delete_n;
+    
+
+    this->m_Capacity = new_capacity;
+}
+
+
+#if 0       /*就放这里 提醒自己吧*/
+template <class T>
+void Uni_array<T>::delete_n(int delete_n)       /*尾删n个*/     /*我突然有个问题。类模板里面能不能有函数模板做成员函数。*/ /*搜了一下 可以*/
+{                                                       /*源赖是new[] 和delete[]必须成对使用。 new[]出来的内存只能整体删除 而不能只删除其中部分*/
+    if(!delete_n)
+        return;
+    int delete_nn = delete_n;
+    int new_capacity = m_Capacity - delete_nn;
+    T* ptr = this->m_ptr;
+    // delete[] ptr;
+    int debug=0;
+    do{
+        delete (ptr+m_Capacity-delete_nn);
+        debug++;
+        delete_nn--;
+    }while(delete_nn!=0);
+
+    this->m_Capacity = new_capacity;
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,8 @@ int main()
         a3.show();
         printf("一阶段测试通过\n");
         pNEWL__;
-        
-
     }
+
     {
         using namespace std;
         Uni_array<Test_class> a1(6);
@@ -47,12 +46,59 @@ int main()
     
     
     
-    // {
-    //     Uni_array<float> a1(6);
-    //     a1.show(6);
-    //     a1[3]+=1;
-    //     printf("\n%f\n",a1[3]);
-    // }
+    {
+        using namespace std;
+
+        Uni_array<float> a1(5);
+        a1[0]+=1;
+        a1[1]+=1;
+        a1[2]+=1;
+        a1[3]+=1;
+        a1[4]+=1;
+        a1.append_n(3);
+        printf("now capacity become:%d \n",a1.m_Capacity);
+        
+
+        a1[5]+=5;
+        a1[6]+=6;
+        a1[7]+=7;
+
+        pNEWL__;a1.show();
+
+    }
+
+    {
+        Uni_array<int> a(5);
+        a[0]+=1;
+        a[1]+=1;
+        a[2]+=1;
+        a[3]+=0;
+        a[4]+=0;
+
+        a.delete_n(2);
+        std::cout << "now capacity:"<<a.m_Capacity;pNEWL__;
+    }
+
+    if(0)   /*这段代码的输出结果表示 m_ptr+1会按输入类型<T>的size递增*/
+    {
+        Uni_array<int> a1(5);
+        std::cout<< "size of Uni_array<int>:" << sizeof(Uni_array<int>);pNEWL__;
+        std::cout<< "a1.mptr:"<<a1.m_ptr;pNEWL__;
+        std::cout<< "a1.mptr+1:"<<a1.m_ptr+1;pNEWL__;
+        std::cout<< "a1.mptr+2:"<<a1.m_ptr+2;pNEWL__;
+        std::cout<< "a1.mptr+3:"<<a1.m_ptr+3;pNEWL__;
+
+        /*      输出结果
+    
+        size of Uni_array<int>:16
+        a1.mptr:0xc14460
+        a1.mptr+1:0xc14464
+        a1.mptr+2:0xc14468
+        a1.mptr+3:0xc1446c
+    
+        */
+
+    }
 
     return 0;
 }
