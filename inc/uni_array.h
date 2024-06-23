2beaa4f9e93d833423be2e1663ae2bc8c4de80a9
xcs <evtjk@hotmail.com>
2024-06-23 17:12:09 +0800

    3rd commit. finish operator[] reload.

--- inc/uni_array.h
+++ inc/uni_array.h
@@ -4,38 +4,55 @@
 #include <iostream>
 #include <string>
 
-template <class T1>
+template <class T>
 class Uni_array
 {
 
 public:
-    T1*  m_ptr;
+    T*  m_ptr;
     unsigned int m_Capacity;
-
+    // unsigned int ; 
     Uni_array(int );
     ~Uni_array();
     void show(int);
+    T& operator[] (int index);
+
+private:
+    T& visit(int index);
+
+
 };
 
-template <class T1>
-Uni_array<T1>::Uni_array(int n)
+template <class T>
+Uni_array<T>::Uni_array(int n)
 {
     m_Capacity = n;
-    m_ptr =  new T1[n]();
+    m_ptr =  new T[n]();/*分配堆空间并赋初值为0；*/
 }
 
-template <class T1>
-Uni_array<T1>::~Uni_array()
+template <class T>
+Uni_array<T>::~Uni_array()
 {
-
+    delete[] this->m_ptr;
 }
-template <class T1>
-void Uni_array<T1>::show(int n)
+template <class T>
+void Uni_array<T>::show(int n)
 {
     for(int i=0;i<n;i++)
         std::cout << m_ptr[i]<<std::endl;
 }
 
 
-#include "uni_array.cpp"
+template <class T>
+T& Uni_array<T>::visit(int index)
+{
+    return m_ptr[index];
+}
+
+template <class T>
+T& Uni_array<T>::operator[] (int index)
+{
+    return m_ptr[index];
+}
+
 #define UNI_ARRAY_CPP__
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ int main()
 {
     Uni_array<int> a1(5);
     a1.show(5);
+    a1[2]+=1;
+    printf("\n%d\n",a1.[2]);
     return 0;
 }
 
